879738c22bfdc6e64613469e97648715be8bb376
Igor Proshyn <101563289+itmwebdev@users.noreply.github.com>
2023-08-30 18:53:41 +0200

    Start of development

--- src/assets/styles/GlobalStyle.js
+++ src/assets/styles/GlobalStyle.js
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800&display=swap');
+    html {
+      font-family: 'Nunito', sans-serif;
+    }
 `;
+
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from 'styled-components';
 import {GlobalStyle} from "../assets/styles/GlobalStyle";
+import {Helmet} from "react-helmet";
 import style from 'index.css';
 
+
+
 const Root = () => {
+    const metaData = {
+        title: 'Default Title',
+        description: ' My Default Description'
+    }
+
+    const metaTitle = {
+        title: 'My title'
+    }
+    useEffect(() => {
+    //    Sanity API
+    }, []);
+
     return (
         <>
-            <GlobalStyle/>
-            <h1 className=''>
-
-            </h1>
+            <Helmet>
+                <title>{metaTitle.title}</title>
+                <meta name="description" content={metaData.description} />
+                <GlobalStyle/>
+            </Helmet>
         </>
     )
 }
